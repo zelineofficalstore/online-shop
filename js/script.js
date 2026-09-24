876b1451f5cbe78c5297fa47ec4a79ead83c4ee3
zelineofficalstore <zelineofficalstore@gmail.com>
2026-09-24 18:18:16 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,94 +1,93 @@
 // Toggle class active untuk hamburger menu
-const navbarNav = document.querySelector(".navbar-nav");
+const navbarNav = document.querySelector('.navbar-nav');
 // ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
 };
 
 // Toggle class active untuk search form
-const searchForm = document.querySelector(".search-form");
-const searchBox = document.querySelector("#search-box");
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
 
-document.querySelector("#search-button").onclick = (e) => {
-  searchForm.classList.toggle("active");
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
   searchBox.focus();
   e.preventDefault();
 };
 
 // Toggle class active untuk shopping cart
-const shoppingCart = document.querySelector(".shopping-cart");
-document.querySelector("#shopping-cart-button").onclick = (e) => {
-  shoppingCart.classList.toggle("active");
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
   e.preventDefault();
 };
 
 // Klik di luar elemen
-const hm = document.querySelector("#hamburger-menu");
-const sb = document.querySelector("#search-button");
-const sc = document.querySelector("#shopping-cart-button");
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
-document.addEventListener("click", function (e) {
+document.addEventListener('click', function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
+    navbarNav.classList.remove('active');
   }
 
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
-    searchForm.classList.remove("active");
+    searchForm.classList.remove('active');
   }
 
   if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
-    shoppingCart.classList.remove("active");
+    shoppingCart.classList.remove('active');
   }
 });
 
 // Modal Box
 // Buka modal
-const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
 
 itemDetailButtons.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
+  btn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const modal = document.querySelector(btn.getAttribute("href"));
+    const modal = document.querySelector(btn.getAttribute('href'));
 
     if (modal) {
-      modal.style.display = "flex";
+      modal.style.display = 'flex';
     }
   });
 });
 
 // Tutup modal
-const closeIcons = document.querySelectorAll(".close-icon");
+const closeIcons = document.querySelectorAll('.close-icon');
 
 closeIcons.forEach((icon) => {
-  icon.addEventListener("click", (e) => {
+  icon.addEventListener('click', (e) => {
     e.preventDefault();
 
-    icon.closest(".modal").style.display = "none";
+    icon.closest('.modal').style.display = 'none';
   });
 });
 
 // Klik di luar modal
-window.addEventListener("click", (e) => {
-  document.querySelectorAll(".modal").forEach((modal) => {
+window.addEventListener('click', (e) => {
+  document.querySelectorAll('.modal').forEach((modal) => {
     if (e.target === modal) {
-      modal.style.display = "none";
+      modal.style.display = 'none';
     }
   });
 });
 
 //Shopping-Cart Navbar
 const cart = [];
-const addToCartButtons = document.querySelectorAll(".add-to-cart");
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
 addToCartButtons.forEach((button) => {
-  button.addEventListener("click", function (e) {
+  button.addEventListener('click', function (e) {
     e.preventDefault();
-    const productCard = this.closest(".product-card");
-    const name = productCard.querySelector("h3").textContent;
-    const image = productCard.querySelector("img").src;
-    const priceText =
-      productCard.querySelector(".product-price").childNodes[0].textContent;
-    const cleanedPrice = priceText.replace("IDR", "").replace("K", "").trim();
+    const productCard = this.closest('.product-card');
+    const name = productCard.querySelector('h3').textContent;
+    const image = productCard.querySelector('img').src;
+    const priceText = productCard.querySelector('.product-price').childNodes[0].textContent;
+    const cleanedPrice = priceText.replace('IDR', '').replace('K', '').trim();
     const price = parseInt(cleanedPrice) * 1000;
     const itemExist = cart.find((item) => item.name === name);
     if (itemExist) {
@@ -105,10 +104,10 @@ addToCartButtons.forEach((button) => {
   });
 });
 function renderCart() {
-  const cartItems = document.getElementById("cart-items");
-  const cartTotal = document.getElementById("cart-total");
-  const badge = document.querySelector(".quantity-badge");
-  cartItems.innerHTML = "";
+  const cartItems = document.getElementById('cart-items');
+  const cartTotal = document.getElementById('cart-total');
+  const badge = document.querySelector('.quantity-badge');
+  cartItems.innerHTML = '';
   let total = 0;
   let totalQty = 0;
   cart.forEach((item, index) => {
@@ -124,7 +123,7 @@ function renderCart() {
 <h3>${item.name}</h3>
  
 <div class="item-price">
-${item.qty} x IDR ${item.price.toLocaleString("id-ID")}
+${item.qty} x IDR ${item.price.toLocaleString('id-ID')}
 </div>
  
 </div>
@@ -138,7 +137,7 @@ Hapus
 </div>
 `;
   });
-  cartTotal.textContent = "IDR " + total.toLocaleString("id-ID");
+  cartTotal.textContent = 'IDR ' + total.toLocaleString('id-ID');
   badge.textContent = totalQty;
 }
 function removeItem(index) {
@@ -146,16 +145,15 @@ function removeItem(index) {
   renderCart();
 }
 
-const modalButtons = document.querySelectorAll(".add-to-cart-modal");
+const modalButtons = document.querySelectorAll('.add-to-cart-modal');
 modalButtons.forEach((button) => {
-  button.addEventListener("click", function (e) {
+  button.addEventListener('click', function (e) {
     e.preventDefault();
-    const modalContent = this.closest(".modal-content");
-    const name = modalContent.querySelector("h3").textContent;
-    const image = modalContent.querySelector("img").src;
-    const priceText =
-      modalContent.querySelector(".product-price").childNodes[0].textContent;
-    const cleanedPrice = priceText.replace("IDR", "").replace("K", "").trim();
+    const modalContent = this.closest('.modal-content');
+    const name = modalContent.querySelector('h3').textContent;
+    const image = modalContent.querySelector('img').src;
+    const priceText = modalContent.querySelector('.product-price').childNodes[0].textContent;
+    const cleanedPrice = priceText.replace('IDR', '').replace('K', '').trim();
     const price = parseInt(cleanedPrice) * 1000;
     const itemExist = cart.find((item) => item.name === name);
     if (itemExist) {
@@ -173,15 +171,15 @@ modalButtons.forEach((button) => {
 });
 
 //Shopping-Cart Menu
-const menuButtons = document.querySelectorAll(".menu-card .add-to-cart");
+const menuButtons = document.querySelectorAll('.menu-card .add-to-cart');
 menuButtons.forEach((button) => {
-  button.addEventListener("click", function (e) {
+  button.addEventListener('click', function (e) {
     e.preventDefault();
-    const menuCard = this.closest(".menu-card");
-    const name = menuCard.querySelector(".menu-card-title").textContent;
-    const image = menuCard.querySelector(".menu-card-img").src;
-    const priceText = menuCard.querySelector(".menu-card-price").textContent;
-    const cleanedPrice = priceText.replace("IDR", "").replace("K", "").trim();
+    const menuCard = this.closest('.menu-card');
+    const name = menuCard.querySelector('.menu-card-title').textContent;
+    const image = menuCard.querySelector('.menu-card-img').src;
+    const priceText = menuCard.querySelector('.menu-card-price').textContent;
+    const cleanedPrice = priceText.replace('IDR', '').replace('K', '').trim();
     const price = parseInt(cleanedPrice) * 1000;
     const itemExist = cart.find((item) => item.name === name);
     if (itemExist) {
@@ -197,3 +195,25 @@ menuButtons.forEach((button) => {
     renderCart();
   });
 });
+
+// Modal Pembayaran
+checkoutBtn.addEventListener("click", () => {
+  document.getElementById("cart-container").innerHTML = `
+<h2>Informasi Pembayaran</h2>
+ 
+<p>Total Belanja: <strong>Rp ${total}</strong></p>
+ 
+<div class="payment-info">
+<p><strong>Bank:</strong> BCA</p>
+<p><strong>No. Rekening:</strong> 1234567890</p>
+<p><strong>Atas Nama:</strong> Nahda Chairunisa</p>
+</div>
+ 
+<p>
+Setelah pembayaran kirim bukti transfer ke WhatsApp:
+<strong>085888180583</strong>
+</p>
+`;
+});
+
+
